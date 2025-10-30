6931503b3a7b7455cfe4cd7fce937da47ce914e7
Talia-R <natalia.1067@hotmail.com>
2025-10-30 08:46:23 -0300

    added lucide icons

--- chronos-pomodoro/src/components/Heading.tsx
+++ chronos-pomodoro/src/components/Heading.tsx
@@ -2,6 +2,20 @@ type HeadingProps = {
     children : string
 }
 
-export function Heading(props: HeadingProps){
-    return <h1>{props.children}</h1>;
+export function Heading({ children }: HeadingProps){
+    return <h1>{children}</h1>;
 }
+
+// acima é a mesma coisa que:
+
+// export function Heading(props: HeadingProps){
+//     const {children} = props
+//     return <h1>{children}</h1>;
+// } isso é desestruturização, mesma coisa que:
+
+
+
+// export function Heading(props: HeadingProps){
+//     const children = props.children
+//     return <h1>{children}</h1>;
+// } 
