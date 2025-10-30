106234aa2a00e94d064df7cd81378e0013781b26
Talia-R <natalia.1067@hotmail.com>
2025-10-30 20:01:05 -0300

    updated and added components

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,9 +1,15 @@
 import { Heading } from "./components/Heading";
 import { Container } from "./components/Container";
-import { IconsSection } from "./components/IconsSection"
-import { Timer } from "./components/Timer"
-import { Title } from "./components/Title";
-import { Form } from "./components/Form"
+import { Menu } from "./components/Menu"
+import { Countdown } from "./components/Countdown"
+import { Logo } from "./components/Logo";
+import { Input } from "./components/Input"
+import { Summary } from "./components/Summary"
+import { Cycle } from "./components/Cycle"
+import { Button } from "./components/Button";
+import { Footer } from "./components/Footer";
+import { PlayIcon } from "lucide-react";
+
 
 
 import './styles/theme.css';
@@ -13,12 +19,20 @@ export function App(){
     return (
         <>
             <Container>
-                <Title/>
-                <IconsSection/>
-                <Timer/>
-                <Form>
-                    task
-                </Form>
+                <Logo/>
+                <Menu/>
+                <Countdown/>
+                <form>
+                    <div>
+                        <Input description="task" id="task" type="text"/>
+                    </div>
+                    <Summary/>
+                    <Cycle/>
+                    <Button>
+                        <PlayIcon/>
+                    </Button>
+                </form>
+                <Footer/>
             </Container>
         </>
     )
