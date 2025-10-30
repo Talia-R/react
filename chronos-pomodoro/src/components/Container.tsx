8cbef15f2521c210a306fe89446b5966a699bccf
Talia-R <natalia.1067@hotmail.com>
2025-10-30 12:20:33 -0300

    added Container component

---
+++ chronos-pomodoro/src/components/Container.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './Container.module.css'
+
+type ContainerProps = {
+    children: React.ReactNode;
+}
+
+export function Container({ children } : ContainerProps){
+    return <div className={styles['main-container']}>{children}</div>
+}
