346209b9e6df4c760eab35051810914a73a33808
Talia-R <natalia.1067@hotmail.com>
2025-10-30 07:11:29 -0300

    updated color themes

---
+++ chronos-pomodoro/src/App.tsx
@@ -0,0 +1,18 @@
+import { Heading } from "./components/Heading";
+
+import './styles/theme.css';
+import './styles/global.css';
+
+export function App(){
+    return (
+        <>
+            <Heading> 
+                Olá mundo
+            </Heading>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates mollitia, eius corrupti facere rem 
+                dolor consequuntur enim illum temporibus ratione quia cumque atque aspernatur neque, sint beatae modi, 
+                quos earum.
+            </p>
+        </>
+    )
+}
