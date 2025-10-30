e8c428fe6433bb32a2bf16d4bcd2082bb0212f79
Talia-R <natalia.1067@hotmail.com>
2025-10-30 12:20:19 -0300

    added IconSection component

---
+++ chronos-pomodoro/src/components/IconsSection.tsx
@@ -0,0 +1,21 @@
+import { History, HomeIcon, Settings, Sun } from "lucide-react";
+import style from './IconsSection.module.css'
+
+export function IconsSection(){
+    return(
+        <div className={style['icones-container']}>
+            <div>
+                <HomeIcon/>
+            </div>
+            <div>
+                <History/>
+            </div>
+            <div>
+                <Settings/>
+            </div>
+            <div>
+                <Sun/>
+            </div>
+        </div>
+    )
+}
