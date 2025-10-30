869ec7a6cfa6f1b18afbd37cd3f0abc4dd3ea942
Talia-R <natalia.1067@hotmail.com>
2025-10-30 20:02:34 -0300

    practiced declaring props type and using destructure

---
+++ chronos-pomodoro/src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css"
+
+type ButtonProps = {
+    children : React.ReactNode;
+}
+
+export function Button( { children } : ButtonProps){
+    return (
+        <button className={styles.button}>{ children }</button>
+    )
+}
