bd1a26bc34a35afcfaa61708c3c20164b318f451
Talia-R <natalia.1067@hotmail.com>
2025-11-01 11:34:24 -0300

    added template

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,27 +1,10 @@
-import { useState } from "react";
-import { PlayIcon } from "lucide-react";
-
-import { Heading } from "./components/Heading";
-import { Container } from "./components/Container";
-import { Menu } from "./components/Menu"
-import { Countdown } from "./components/Countdown"
-import { Logo } from "./components/Logo";
-import { Footer } from "./components/Footer";
-import { MainForm } from "./components/MainForm";
+import { Home } from "./pages/Home";
 
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App(){
-    return (
-        <>
-            <Container>
-                <Logo/>
-                <Menu/>
-                <Countdown/>
-                <MainForm/>
-                <Footer/>
-            </Container>
-        </>
+    return (    
+        <Home/>
     )
 }
