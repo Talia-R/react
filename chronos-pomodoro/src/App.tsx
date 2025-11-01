4b6ddf44b7b738498506c70560ebe4e450348574
Talia-R <natalia.1067@hotmail.com>
2025-11-01 10:40:09 -0300

    Created new component MainForm and updated App

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,15 +1,13 @@
+import { useState } from "react";
+import { PlayIcon } from "lucide-react";
+
 import { Heading } from "./components/Heading";
 import { Container } from "./components/Container";
 import { Menu } from "./components/Menu"
 import { Countdown } from "./components/Countdown"
 import { Logo } from "./components/Logo";
-import { Input } from "./components/Input"
-import { Summary } from "./components/Summary"
-import { Cycle } from "./components/Cycle"
-import { Button } from "./components/Button";
 import { Footer } from "./components/Footer";
-import { PlayIcon } from "lucide-react";
-import { useState } from "react";
+import { MainForm } from "./components/MainForm";
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -21,16 +19,7 @@ export function App(){
                 <Logo/>
                 <Menu/>
                 <Countdown/>
-                <form>
-                    <div>
-                        <Input labelText="task" id="task" type="text" placeholder="Digite algo"/>
-                    </div>
-                    <Summary/>
-                    <Cycle/>
-                    <Button>
-                        <PlayIcon/>
-                    </Button>
-                </form>
+                <MainForm/>
                 <Footer/>
             </Container>
         </>
---
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -0,0 +1,20 @@
+import { PlayIcon } from "lucide-react";
+import { Button } from "../Button";
+import { Cycle } from "../Cycle";
+import { Input } from "../Input";
+import { Summary } from "../Summary";
+
+export function MainForm(){
+    return (
+        <form>
+            <div>
+            <Input labelText="task" id="task" type="text" placeholder="Digite algo"/>
+            </div>
+            <Summary/>
+            <Cycle/>
+            <Button>
+            <PlayIcon/>
+            </Button>
+        </form>
+    )
+}
