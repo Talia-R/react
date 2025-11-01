81192edd63499b62237271b5297478e2e2697d19
Talia-R <natalia.1067@hotmail.com>
2025-11-01 10:30:03 -0300

    added settings to localStorage and added table for icons

--- chronos-pomodoro/src/components/Menu/index.tsx
+++ chronos-pomodoro/src/components/Menu/index.tsx
@@ -1,24 +1,34 @@
-import {  HistoryIcon, HomeIcon, SettingsIcon, SunIcon } from "lucide-react";
+import {  HistoryIcon, HomeIcon, MoonIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from './styles.module.css'
 import { useState, useEffect } from "react";
 
 type AvailableThemes = 'dark' | 'light';
-
 export function Menu(){
-    const [theme, setTheme] = useState<AvailableThemes>('dark');
+    const [theme, setTheme] = useState<AvailableThemes>(() => {
+        const storageTheme = localStorage.getItem("theme") as AvailableThemes || "dark"
+        return storageTheme;
+
+    });
+
+    const nextThemeIcon = {
+        dark: <SunIcon/>,
+        light: <MoonIcon/>
+
+    }
 
     function handleThemeChange(
         event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
     ){
         event.preventDefault();
-        console.log("clicou")
 
         setTheme(prevTheme =>prevTheme === 'dark' ? 'light' : 'dark')
     }
-    
+
+
     useEffect(() => {
         document.documentElement.setAttribute('data-theme', theme)
-    })
+        localStorage.setItem('theme', theme)
+    }, [theme]);
 
     return(
         <nav className={styles['menu-container']}>
@@ -48,7 +58,7 @@ export function Menu(){
             aria-label="Mudar o tema" 
             title="Tema" 
             onClick={handleThemeChange}>
-                <SunIcon/>
+                {nextThemeIcon[theme]}
             </a>
 
         </nav>
